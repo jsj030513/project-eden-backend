b7e132d67161075afb09f7858aa393b470d20ae7
장성주 <jangseongju@jangseongjuui-MacBookAir.local>
2026-07-06 06:32:45 +0900

    chore: initialize Project Eden backend

---
+++ src/main/java/com/projecteden/ProjectEdenBackendApplication.java
@@ -0,0 +1,13 @@
+package com.projecteden;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectEdenBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectEdenBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projecteden/ProjectEdenBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projecteden;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectEdenBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
